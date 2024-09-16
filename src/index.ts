e7c774acf09df2dcfcacf8582d6a1c8166f379de
Charles Opute Odili <chaluwa@gmail.com>
2024-09-16 13:30:38 -0600

    update problem statement

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,15 @@ app.get("/", (req: Request, response: Response) => {
 	// this added field is fine
     console.log(req.userId);
 
-    response.status(200).added('something').json({ message: 'Hello!' });
+    response
+        .status(200)
+        .added('something')
+        .json({ message: 'Hello!' })
+        .added('something else');
+
     // since src/@types/index.d.ts sets .status() to return void
     // I was hoping we should not be able to call .added()
-    
-    // Also, from the line below, the return of .json() should not be callable
-    // response.status(200).json({ message: 'Hello!' }).added('something');
+    // Also, the .json() should not be callable nor its return type
 });
 
 const PORT = 3000;
